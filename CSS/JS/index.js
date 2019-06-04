d9ea96ed09fd5dae62f50bc9428a703aca7c8952
JoshuaVandergrif <VandiTechnician@gmail.com>
2019-06-03 19:49:02 -0500

    Fixed UI bug on the 500px mobile platform.

--- CSS/JS/index.js
+++ CSS/JS/index.js
@@ -1,4 +1,6 @@
 //Event Listener Created
-const btnEvent = document.getElementById('button-class').addEventListener('click', function(event) {
+const btnEvent = document.getElementById('primary').btnEvent.addEventListener('click', function(event) {
   console.log(`I was clicked and this is my event: ${event}`)
 });
+
+
