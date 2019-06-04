d68f829f4f932b0cf35404ab9f644a7984cbef8c
JoshuaVandergrif <VandiTechnician@gmail.com>
2019-06-03 19:25:46 -0500

    Trying to debug JS code..

--- CSS/JS/index.js
+++ CSS/JS/index.js
@@ -1,9 +1,4 @@
-//Added an event listener
-//Event listener
-
-const btn = document.getElementById("primary").addEventListener("click", displayDate);
-
-function displayDate() {
-    document.getElementById("demo").innerHTML = Date();
-}
-console.log(displayDate)
+//Event Listener Created
+const btnEvent = document.getElementById('button-class').addEventListener('click', function(event) {
+  console.log(`I was clicked and this is my event: ${event}`)
+});
